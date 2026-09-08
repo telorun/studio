4e63b1d691f5b8cd00766869006afd35c9fd5cb3
bpasinski <bpasinski@users.noreply.github.com>
2026-09-08 16:25:41 +0000

    deploy: telorun/telo@8cfb01c20f2299a5c3e37365b71079fae2a884fe

---
+++ assets/index-CFdneV_M.js
@@ -0,0 +1,1 @@
+import{i}from"./index-D6gxx9CD.js";async function o(e={}){return typeof e=="object"&&Object.freeze(e),await i("plugin:dialog|open",{options:e})}export{o as open};
--- assets/index-ChM2hZrC.js
+++
@@ -1,1 +0,0 @@
-import{i}from"./index-Cm8FK6RJ.js";async function o(e={}){return typeof e=="object"&&Object.freeze(e),await i("plugin:dialog|open",{options:e})}export{o as open};
